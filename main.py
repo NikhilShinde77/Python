3353bb91ab18cdb589a4bf6e3eda8d85c364ef64
NikhilShinde77 <nikhilshinde15651@gmail.com>
2026-07-20 19:35:18 +0530

    add python

---
+++ main.py
@@ -0,0 +1,4 @@
+print("hi my name is nikhi")
+print("i love janu")
+'''hi my name is nikhill but i getmy fist ove i
+give ,me firat pic of the same theat get ok'''
---
+++ p1.py
@@ -0,0 +1,211 @@
+"""input/output"""
+# programe 
+# a=float(input("enter the nuber 1:-"))
+# b=float(input("enter the nuber 2:-"))
+# print(a>=b)
+
+
+
+
+# num1=float(input("enter the nuber 1:-"))
+# num2=float(input("enter the nuber 2:-"))
+# print("avg of number ",(num1+num2)/2);
+
+
+
+# sq=float(input("enter side :-"))
+# print("area of sq is",sq*sq5
+#       )
+# l=int(input("enter lanth of squer:-"))
+# b=int(input("enter.brash og sqer:-"))
+# sq=l*b
+# print("sq are is :-",sq)
+ 
+# num1=int(input("enter the number 1 :"))
+# num2=int(input("enter the number 2 :"))
+# sum=num1+num2
+# print("sum of two number",sum)
+
+
+
+# name=input("enter name")
+# age=int(input("enter age"))
+# mark=int(input("enter marks"))
+
+# print("walcom to",name)
+# print("crent age is ",age)
+# print("maks",mark)
+
+
+
+
+
+
+# age=input("enter age:-")
+# print(type(age))
+# print("you age is :",age)
+
+# age=int(input("enter age:-"))
+# print(type(age))
+# print("you age is :",age)
+
+
+# name=input("enter your name :-")
+# print("welcom",name)
+
+
+
+"""type conversion"""
+# a=int("20")
+# b=10
+
+# sum=a+b
+# print(sum)
+
+
+
+
+
+
+
+
+
+
+
+"""Logical Operators"""
+# a=10
+# b=2
+# print(not a>b)
+# a=True
+# b=False
+# print(a and b)
+# print(a or b)
+
+
+
+
+
+
+
+
+
+
+
+
+# """Assignment Operators"""
+
+
+# a=10
+# a**=5
+# print(a)
+
+
+
+
+
+
+
+
+# """relans oprater"""
+
+# a=10
+# b=1
+# # print(a==b)
+# # print(a!=b)
+# # print(a>b)
+# # print(a>=b)
+# print(a<b)
+# print(a<=b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""arithmatic """
+# a=5
+# b=5
+# print("add",(a+b))
+# print("sub",(a-b))
+# print("mull",(a*b))
+# print("div",(a/b))
+# print("remander",(a%b))
+# print(a**b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# num1=int(input("enter the number"))
+# num2=int(input("enter the number"))
+# add=num1+num2
+# sub=num1-num2
+# mul=num1*num2
+# div=num1/num2
+# print(add)
+# print(sub)
+# print(mul)
+# print(div)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name="shinde nikhil"
+# age=19
+# price=20.0
+# iam=True
+# iam1=False
+# name1='ns'
+# name2="ns"
+# name3="""ns"""
+"""using string """
+
+# print(name1,name2,name3)
+
+
+
+"""data type"""
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# print(type(iam))
+# print(type(iam1))
+""""print using """
+# print(name)
+# print(age)
+# print(price)
+# print("my name is:",name)
+# print("my age is:",age)
+# print("my crebt pone prices is",price)
