e7af457d67101f04345f1805741a254ded17ab32
NikhilShinde77 <nikhilshinde15651@gmail.com>
2026-07-22 17:23:41 +0530

    day two in basic python

---
+++ p2.py
@@ -0,0 +1,304 @@
+x=int(input("number:-"))
+if(x%7==0):
+  print("Mulltipal of 7")
+else:
+  print("not multipal of 7")
+
+
+
+
+
+
+
+# a=int(input("enter number 1:"))
+# b=int(input("enter number 2:"))
+# c=int(input("enter number 3:"))
+
+# if(a>b and a>c):
+#   print("a is big")
+# elif(b>c):
+#   print("b is big")
+# else:
+#   print("c is big1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# num=int(input("enter the number:- "))
+# if(num%2==0):
+#   print("number is even ")
+# else:
+#   print("number is Odd")
+
+
+
+
+
+
+
+
+
+
+
+
+# age=int(input("enter your age:- "))
+# if(age>=18):
+#   if(age>=80):
+#     print("can not drive")
+#   else:
+#     print("can drive")
+# else:
+#   print("can not deive")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# marks=int(input("enter your marks:- "))
+# if(marks>=90):
+#   print("GRADE  A")
+# elif(marks < 90 and marks >=80):
+#   print("GRADE  B")
+# elif(marks < 80 and marks >=70):
+#   print("GRADE  C")
+# elif(marks < 70 and marks>=36):
+#   print("GRADE  D")
+# else:
+#   print("File")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# age=int(input("enter the youir age:- "))
+# if(age>=18):
+#   print("can vote")
+# else:
+#   print("CANNOT vote")
+
+
+
+
+
+
+
+
+
+
+
+
+# light=input("enter:-")
+# if(light=="red"):
+#   print("stope")
+# elif(light=="green"):
+#   print("go....")
+# elif(light=="yello"):
+#   print("look")
+# else:
+#   print("the rafic lit is brokane")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# age=int(input("enter your age:-"))
+# if(age>=18):
+#   print("you can vote")
+#   print("you can drive")
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""conditional Statements"""
+# str=input("enter the name")
+# print(len(str))
+
+
+# str="hi"
+# # print(str.endswith("f"))
+# # print(str.capitalize())
+# # print(str.replace("i","k"))
+# print(str)
+
+ 
+
+
+
+
+
+
+
+
+# str="nikhil"
+# print(str[0:6])
+# print(str[-6:])
+
+
+
+
+
+
+
+
+
+
+
+# str="nikhil"
+# print(str[0])
+# print(str[1])
+# print(str[2])
+# print(str[3])
+# print(str[4])
+# print(str[5])
+
+
+
+
+
+
+
+
+
+
+
+
+# first_str="hi"+""+"nikhil"
+# print(len(first_str))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# str="nikhil"
+# print(len(str))
+
+
+
+# str1="mmy name is "
+# str2="nikhil"
+# print(str1+str2)
+
+
+
+# str="my first \n love is"
+# print(str)
+
+
+
+# code="this is single code'smake this "
+# print(code)
+
+
+
+
+# str1="this is my first love"
+# str2='this is my first love'
+# str3="""hi my name is nikhil"""
+
+# print(str3)
